2a110fbc0a784f0ae49948df8724177611be45cf
Ismael Luna <ismaelunalvarez@gmail.com>
2023-07-17 19:49:25 +0200

    Minnor Changes

---
+++ projects/VideoClub/src/const.js
@@ -0,0 +1,3 @@
+const API_KEY = 'cbe7dc0'
+
+export const API_MOVIES_ENDPOINT = `http://www.omdbapi.com/?apikey=${API_KEY}&`
--- projects/VideoClub/src/hooks/useMovies.js
+++ projects/VideoClub/src/hooks/useMovies.js
@@ -1,6 +1,11 @@
+import { useState } from 'react'
 import responseMovies from '../mocks/resultsApiWithSearch.json'
+import { API_MOVIES_ENDPOINT } from '../const'
+import withoutResults from '../mocks/noResults.json'
 
-export function useMovies () {
+export function useMovies ({ search }) {
+  const [responseMoviesApi, setResponseMoviesApi] = useState([])
+
   const movies = responseMovies.Search
   const mappedMovies = movies?.map(movie => ({
     id: movie.imdbID,
@@ -9,5 +14,18 @@ export function useMovies () {
     poster: movie.Poster
   }))
 
-  return { movies: mappedMovies }
+  const urlQuerySearch = `${API_MOVIES_ENDPOINT}s=${search}`
+
+  const getMovies = async () => {
+    if (search) {
+      setResponseMoviesApi(responseMovies)
+    } else {
+      setResponseMoviesApi(withoutResults)
+    }
+    // const res = await fetch(urlQuerySearch)
+    // const data = await res.json()
+    // console.log(data)
+  }
+
+  return { movies: mappedMovies, getMovies }
 }
